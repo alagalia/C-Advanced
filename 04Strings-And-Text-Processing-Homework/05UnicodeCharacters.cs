72e06160f0d0c2525bc69998db38339390b9392a
Галя Георгиева <alice.gospodinova@gmail.com>
2015-05-12 22:16:58 +0300

    commit Problem 05 Unicode Characters

---
+++ 04Strings-And-Text-Processing-Homework/05UnicodeCharacters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        foreach (var chr in input)
+        {
+            Console.Write("\\U{0:x4}", (int)chr);
+        }
+        Console.WriteLine();
+    }
+}

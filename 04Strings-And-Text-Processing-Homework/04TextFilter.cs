b544186ee166daf44dc4275025d428c002e587e3
Галя Георгиева <alice.gospodinova@gmail.com>
2015-05-12 21:43:08 +0300

    Commit Problem 04 Text Filter

---
+++ 04Strings-And-Text-Processing-Homework/04TextFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+class TextFilter
+{
+    static void Main()
+    {
+        string[] banned = Console.ReadLine().Split(new char[] {',',' '},StringSplitOptions.RemoveEmptyEntries);
+        StringBuilder bannedText = new StringBuilder(Console.ReadLine());
+        string[] bannedAsAsterisks = new string[banned.Count()];
+
+        for (int i = 0; i < banned.Count(); i++)
+        {
+            bannedAsAsterisks[i] = new string('*', banned[i].Length);
+            bannedText.Replace(banned[i], bannedAsAsterisks[i]);
+        }
+        Console.WriteLine(bannedText);
+    }
+}

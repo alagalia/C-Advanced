ef7f547a2ce3588077b723c3a6fb8a7c2ccf7d06
Галя Георгиева <alice.gospodinova@gmail.com>
2015-05-12 21:18:13 +0300

    commit problem 02 String Length

---
+++ 04Strings-And-Text-Processing-Homework/02StringLength.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+class StringLength
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string output = string.Empty;
+        if (input.Length > 20)
+        {
+            output = input.Substring(0, 20);
+        }
+        else
+        {
+            StringBuilder outputSB = new StringBuilder();
+            outputSB.Append(input);
+            outputSB.Append('*', 20 - input.Length);
+            output = outputSB.ToString();
+        }
+        Console.WriteLine(output);
+    }
+}

8518c5805ac3a8cbfb4e1f39ceb2ea79af8c2067
Галя Георгиева <alice.gospodinova@gmail.com>
2015-05-12 21:19:36 +0300

    commit Problem 03 Count Substring Occurrences

---
+++ 04Strings-And-Text-Processing-Homework/03CountSubstringOccurrences.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+class CountSubstringOccurrences
+{
+    static void Main()
+    {
+        string input = Console.ReadLine().ToLower();
+        string forSearch = Console.ReadLine();
+        int poss = input.IndexOf(forSearch);
+        int counter = 0;
+        while (poss >= 0 && poss <= input.Length)
+        {
+            counter++;
+            poss = input.IndexOf(forSearch, poss + 1);
+
+        }
+        Console.WriteLine(counter);
+    }
+}

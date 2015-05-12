6d6bd2c3fb48f92c536d38ae05772e9e2b1fc313
Галя Георгиева <alice.gospodinova@gmail.com>
2015-05-12 23:24:05 +0300

    commit Problem 06 Palindromes

---
+++ 04Strings-And-Text-Processing-Homework/06Palindromes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Palindromes
+{
+    static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(new char[] { ',', ':', ';', ' ', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+        List<string> sortedOutput = new List<string>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            string reversed = Reverse(input[i]);
+            if (input[i] == reversed)
+            {
+                sortedOutput.Add(input[i]);
+            }
+        }
+        sortedOutput.Sort();
+        Console.WriteLine(string.Join(", ", sortedOutput));
+    }
+}

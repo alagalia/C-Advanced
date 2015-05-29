08c804db85de5d8c807788daedb608c9c6bb82e5
Галя Георгиева <alice.gospodinova@gmail.com>
2015-05-29 12:48:29 +0300

    Update PhoneNumbers.cs

--- Exam preparation/PhoneNumbers.cs
+++ Exam preparation/PhoneNumbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,75 +11,56 @@ namespace PhoneNumbers
     {
         static void Main()
         {
-            string input = Console.ReadLine();
-            string pattern = @"[^\+\d\[A-Za-z]"; //match all chars without name and phone number
-            StringBuilder finalResult = new StringBuilder("<ol>");
-            bool checkForMatches = false; //if there aren`t matches -> print "<p>No matches!</p>"
+            StringBuilder start = new StringBuilder();
 
+            string input = Console.ReadLine();
             while (input != "END")
             {
-                //replease all char, without name and phone number with " " and then replease all whitespaces with " "
-                string cleared = Regex.Replace(input, pattern, " ").Trim();
-                string cleartByWhiteSpaces = Regex.Replace(cleared, @"\s+", " ");
-
-                //match groups №1 "name"  and №2"phone number"
-                string nameAndPhonePattern = @"([A-z]+)((?:\+*\d*\s*)+)";
-                MatchCollection machesNameAndPhone = Regex.Matches(cleartByWhiteSpaces, nameAndPhonePattern);
-
-                for (int i = 0; i < machesNameAndPhone.Count; i++)
-                {
-                    string name = machesNameAndPhone[i].Groups[1].ToString();
-                    string telephone = RemoveSpace(machesNameAndPhone[i].Groups[2].ToString());
-
-                    if (telephone == " " || telephone == "" || name == " " || name == "" || name[0] < 65 || name[0] > 90)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        finalResult.Append("<li><b>" + machesNameAndPhone[i].Groups[1] + ":</b> " + telephone.Trim() + "</li>");
-                        checkForMatches = true;
-                    }
-                }
-
+                start.Append(input);
                 input = Console.ReadLine();
             }
-            if (!checkForMatches)
+            string text = start.ToString();
+
+            //match groups №1 "name"  and №2"phone number"
+            string nameAndPhonePattern = @"([A-Z][a-zA-Z]*)[^a-zA-Z\+]*?(?=\+|[0-9]{2})([0-9\+]{0,1}[0-9][0-9\/(). -]*[0-9])";
+
+            MatchCollection machesNameAndPhone = Regex.Matches(text, nameAndPhonePattern);
+
+            if (machesNameAndPhone.Count == 0)
             {
                 Console.WriteLine("<p>No matches!</p>");
                 return;
             }
-            else
-            {
-                finalResult.Append(("</ol>"));
-                Console.WriteLine(finalResult);
-            }
-
-        }
 
 
+            StringBuilder finalResult = new StringBuilder("<ol>");
 
-        private static bool CheckIfHasMaches(MatchCollection machesNameAndPhone)
-        {
-            bool checkName = false;
-            bool checkPhone = false;
             for (int i = 0; i < machesNameAndPhone.Count; i++)
             {
-                if (machesNameAndPhone[i].Groups[1].ToString() != "")
-                {
-                    checkName = true;
-                }
-                if (machesNameAndPhone[i].Groups[2].ToString() != "")
-                {
-                    checkName = true;
-                }
-            }
-            return (checkName && checkPhone);
-        }
+                string name = machesNameAndPhone[i].Groups[1].ToString();
+                string telephone = machesNameAndPhone[i].Groups[2].ToString();
 
-        private static string RemoveSpace(string value)
-        {
-            return Regex.Replace(value, @"\s+", "");
+                telephone = telephone.Replace("(", "");
+                telephone = telephone.Replace(")", "");
+                telephone = telephone.Replace("/", "");
+                telephone = telephone.Replace(".", "");
+                telephone = telephone.Replace("-", "");
+                telephone = telephone.Replace(" ", "");
+
+                if (telephone == " " || telephone == "" || name == " " || name == "" || name[0] < 65 || name[0] > 90)
+                {
+                    continue;
+                }
+                else
+                {
+                    finalResult.Append("<li><b>" + machesNameAndPhone[i].Groups[1] + ":</b> " + telephone.Trim() + "</li>");
+                }
+
+            }
+
+            finalResult.Append(("</ol>"));
+            Console.WriteLine(finalResult.ToString());
+
         }
     }
 }
